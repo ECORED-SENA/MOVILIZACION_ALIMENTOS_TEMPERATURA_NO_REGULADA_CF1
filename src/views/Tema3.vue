2648218ff938446f41c287f2e8541e209399df82
maronita <maronita@users.noreply.github.com>
2021-12-02 07:59:09 -0500

    Ajustes

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -35,36 +35,37 @@
       iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     br
     br
-    .row.justify-content-center.mb-5
-      ImagenInfografica.color-secundario.mb-3
+    .jumbotron-10.p-5
+      ImagenInfografica.color-secundario.mb-5
         template(v-slot:imagen)
           figure
             img(src='@/assets/curso/Images/tema3/img_3.svg', alt='', style="width:830px; display:block; margin:0 auto;")
     
-        .tarjeta.color-acento-botones.p-3(x="11%" y="52%" numero="")
+        .tarjeta.color-acento-botones.p-3(x="6%" y="52%" numero="")
           .h5.mb-2 Materias primas
 
-        .tarjeta.color-acento-botones.p-3(x="25%" y="100%" numero="")
+        .tarjeta.color-acento-botones.p-3(x="22%" y="97%" numero="")
           .h5.mb-2 Productos intermedios
 
-        .tarjeta.color-acento-botones.p-3(x="49%" y="100%" numero="")
+        .tarjeta.color-acento-botones.p-3(x="48%" y="97%" numero="")
           .h5.mb-2 Productos terminados
 
-        .tarjeta.color-acento-botones.p-3(x="72%" y="100%" numero="")
+        .tarjeta.color-acento-botones.p-3(x="75%" y="97%" numero="")
           .h5.mb-2 De accesorios
 
-        .tarjeta.color-acento-botones.p-3(x="86%" y="52%" numero="")
+        .tarjeta.color-acento-botones.p-3(x="90%" y="52%" numero="")
           .h5.mb-2 De refracción
-    p.mb-3 <b>Recepción de mercancías, cargue, descargue, traslados</b>
-    .col-lg-12.mx-auto.mb-5
-      .bloque-texto-g.color-custom-2.p-3.p-sm-4.p-md-5.mb-5
-        .bloque-texto-g-1__img(
-          :style="{'background-image': `url(${require('@/assets/curso/Images/tema3/img_4.png')})`}"
-        )
-        .bloque-texto-g-1__texto.p-4
-          p.mb-3 En un depósito logístico, la recepción de mercancías es la transferencia o aceptación en custodia de un producto o casa, entregado por un productor o vendedor, para que este sea trasladado hasta un cliente destinatario, en un tiempo estipulado y bajo las condiciones requeridas para la manipulación del producto.  
+      p.mb-3 <b>Recepción de mercancías, cargue, descargue, traslados</b>
+  
+      .col-lg-12.mx-4.mb-5
+        .bloque-texto-g.color-custom-2.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g-1__img(
+            :style="{'background-image': `url(${require('@/assets/curso/Images/tema3/img_4.png')})`}"
+          )
+          .bloque-texto-g-1__texto.p-4
+            p.mb-3 En un depósito logístico, la recepción de mercancías es la transferencia o aceptación en custodia de un producto o casa, entregado por un productor o vendedor, para que este sea trasladado hasta un cliente destinatario, en un tiempo estipulado y bajo las condiciones requeridas para la manipulación del producto.  
 
-          P El cargue de mercancías es la disposición de la mercancía en el vehículo que la transporta al destino, este proceso debe asegurar aspectos como una adecuada distribución del peso en la unidad de transporte y que la mercancía cargada si este asignada en la ruta que sigue el automotor. ue, de manera simultánea, invita a las personas a agregar valor desde sus puestos de trabajo. 
+            P El cargue de mercancías es la disposición de la mercancía en el vehículo que la transporta al destino, este proceso debe asegurar aspectos como una adecuada distribución del peso en la unidad de transporte y que la mercancía cargada si este asignada en la ruta que sigue el automotor. ue, de manera simultánea, invita a las personas a agregar valor desde sus puestos de trabajo. 
 
 </template>
 
